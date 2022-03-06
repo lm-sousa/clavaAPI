8c51d92f0fe653eed1d79a826a1dc2432ab111b4
L. Sousa <lm.sousa@fe.up.pt>
2022-03-06 14:20:10 +0000

    Convert MathExtra module

--- src/lara/MathExtra.ts
+++ src/lara/MathExtra.ts
@@ -1,59 +1,59 @@
+import { checkType, isJoinPoint } from "./LaraCore.js";
+
+const LaraExtraApiTools = Java.type("pt.up.fe.specs.lara.LaraExtraApiTools");
+
 /**
  * @class
  */
-var MathExtra = {};
+export default class MathExtra {
+    /**
+     * Attempts to simplify a mathematical expression.
+     *
+     * @param {string|$jp} expression - The expression to simplify.
+     * @param {object} constants - An object that maps variable names to constants.
+     *
+     * @returns {string} simplified expression
+     */
+    static simplify(expression: string | any, constants: any) {
+        if (isJoinPoint(expression)) {
+            expression = expression.code;
+        }
 
-/**
- * Attempts to simplify a mathematical expression.
- *
- * @param {string|$jp} expression - The expression to simplify.
- * @param {object} constants - An object that maps variable names to constants.
- *
- * @returns {string} simplified expression
- */
-MathExtra.simplify = function(expression, constants) {
-	if(isJoinPoint(expression)) {
-		expression = expression.code;
-	}
+        checkType(expression, "string", "MathExtra.simplify(), expression");
 
-	checkType(expression, "string", "MathExtra.simplify(), expression");
-	
-	var HashMap = Java.type("java.util.HashMap");            
-	var map = new HashMap();
-	
-	if(constants !== undefined) {
-	
-		for(var p in constants) {
-			map.put(p, constants[p]);
-		}
-	
-	}
-	
-	return LaraExtraApiTools.simplifyExpression(expression, map);
-}
+        var HashMap = Java.type("java.util.HashMap");
+        var map = new HashMap();
 
+        if (constants !== undefined) {
+            for (var p in constants) {
+                map.put(p, constants[p]);
+            }
+        }
 
-/**
- * Attempts to convert a mathematical expression to valid C code (e.g., converts ^ to a call to pow()).
- *
- * @param {string|$jp} expression - The expression to simplify.
- *
- * @returns {string} simplified expression as C code
- */
-MathExtra.convertToC = function(expression) {	
-	return LaraExtraApiTools.convertToC(expression);
-}
+        return LaraExtraApiTools.simplifyExpression(expression, map);
+    }
 
+    /**
+     * Attempts to convert a mathematical expression to valid C code (e.g., converts ^ to a call to pow()).
+     *
+     * @param {string|$jp} expression - The expression to simplify.
+     *
+     * @returns {string} simplified expression as C code
+     */
+    static convertToC(expression: string | any) {
+        return LaraExtraApiTools.convertToC(expression);
+    }
 
-/**
- * Attempts to simplify a mathematical expression, returning a string that represents C code.
- *
- * @param {string|$jp} expression - The expression to simplify.
- * @param {object} constants - An object that maps variable names to constants.
- *
- * @returns {string} simplified expression as C code
- */
-MathExtra.simplifyToC = function(expression, constants) {
-	var simplifiedExpr = MathExtra.simplify(expression, constants);
-	return MathExtra.convertToC(simplifiedExpr);
+    /**
+     * Attempts to simplify a mathematical expression, returning a string that represents C code.
+     *
+     * @param {string|$jp} expression - The expression to simplify.
+     * @param {object} constants - An object that maps variable names to constants.
+     *
+     * @returns {string} simplified expression as C code
+     */
+    static simplifyToC(expression: string | any, constants: any) {
+        var simplifiedExpr = MathExtra.simplify(expression, constants);
+        return MathExtra.convertToC(simplifiedExpr);
+    }
 }
