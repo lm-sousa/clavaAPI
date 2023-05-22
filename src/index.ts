42e7550c7a6148118f1d68c432c972a424ae9164
L. Sousa <lm.sousa@fe.up.pt>
2023-05-22 15:42:53 +0100

    Added unit test capability to the project

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export function IsNullOrEmpty(str: string): boolean {
-  return str == null || str.trim().length === 0;
+export function IsNullOrEmpty(str: any): boolean {
+    return str == null || str.trim().length === 0;
 }
